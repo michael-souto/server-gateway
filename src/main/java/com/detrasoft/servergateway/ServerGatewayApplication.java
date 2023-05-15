89504c5e7a0ef033e3e28959dba6e47ee608594d
Michael Souto <michaelsouto@outlook.com>
2023-05-14 23:20:19 -0300

    implementing source control key

--- src/main/java/com/detrasoft/servergateway/ServerGatewayApplication.java
+++ src/main/java/com/detrasoft/servergateway/ServerGatewayApplication.java
@@ -2,8 +2,10 @@ package com.detrasoft.servergateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.detrasoft")
 public class ServerGatewayApplication {
 
 	public static void main(String[] args) {
